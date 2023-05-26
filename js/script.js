6410defe7d8a6bd3267ba86d84f563a322162f12
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-26 14:42:22 +0200

    Question check & originary finalPrice calc

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // Console check
 console.log("JS OK");
+
+
+// Mod targeting element
+const targetElement = document.getElementById("target");
+console.log(targetElement)
+
+// Kilometers check
+const userKM = parseInt(prompt("Inserire il numero di chilometri" ,"50"))
+
+// Age Check
+const userAge = parseInt(prompt("Inserire l'età del passeggero", "25"))
+
+
+// Operazione di calcolo del costo
+
+const finalPrice = (userKM * 0.21)
+

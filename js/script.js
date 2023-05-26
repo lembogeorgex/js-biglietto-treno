3fe2ca19d59fad7a9bccd8c842f3cde2d909814a
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-26 15:55:17 +0200

    last version

--- js/script.js
+++ js/script.js
@@ -14,31 +14,28 @@ const userAge = parseInt(prompt("Inserire l'età del passeggero", "25"))
 
 
 
+// Calcolo del prezzo standard
+const  standardPrice = (userKM * 0.21)
+// Calcolo dello sconto 
+let discountYoungPrice
+let discountSeniorprice
+if (userAge < 18) {
+    discountYoungPrice = standardPrice * 20 / 100
+}
+else if (userAge > 65) {
+    discountSeniorPrice = standardPrice * 40 / 100
+}
 
-// Calcolo dei prezzi scontati
-
-const finalPrice = (userKM * 0.21)
-console.log(finalPrice)
-
-const youngPrice = (finalPrice * 20 / 100)
-console.log(youngPrice)
-
-const 
-// // Operazione di calcolo del costo
-
-// let finalPrice
-
-// if (userAge > 18) {
-//     finalPrice = (userKM * 0.21 - finalPrice * 20 / 100)
-//     console.log(finalPrice)
-// }
-
-// else if  (userAge < 65) {
-//     finalPrice = (userKM * 0.21 - finalPrice * 40 / 100)
-//     console.log(finalPrice)
-// }
-//     else {
-//         finalPrice = (userKM * 0.21)
-//     console.log(finalPrice)
-    
-// }
+// Applicazione dello sconto
+let finalPrice
+if (userAge < 18) {
+  finalprice = (standardPrice - discountYoungPrice)
+  console.log(finalPrice)
+}
+else if (userAge > 65) {
+    finalprice = (standardPrice - discountSeniorPrice)
+    console.log(finalPrice)
+}
+else{
+    console.log(standardPrice)
+}
